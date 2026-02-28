ee5f0d483fa4c9d11ec45c07a8b980c6b3610c84
saba javakhishvili <69425498+javaxa01@users.noreply.github.com>
2026-02-28 19:59:11 +0400

    Initial Commit

---
+++ TasklyLastProject/TasklyLastProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyLastProjectApp.swift
+//  TasklyLastProject
+//
+//  Created by Saba Javakhishvili on 28.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TasklyLastProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
